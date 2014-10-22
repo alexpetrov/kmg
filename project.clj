2a7dbdbad9feeeaf0278581ede9ac86f57d7dfb3
alexpetrov <alexpetrov.rb@gmail.com>
2014-10-22 15:05:10 +0400

    Update some dependencies. Switched to :acvanced optimization of cljsbuild

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2234"]
-                 [com.datomic/datomic-free "0.9.4894"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [com.datomic/datomic-free "0.9.4956"]
                  [datomic-schema-grapher "0.0.1"]
                  [enfocus "2.1.0-SNAPSHOT"]
                  [compojure "1.1.8"]
                  [sonian/carica "1.1.0" :exclusions [[cheshire]]]
                  [fogus/ring-edn "0.2.0"]
                  [cljs-ajax "0.2.3"]
-                 [com.taoensso/timbre "2.7.1"]
+                 [com.taoensso/timbre "3.3.1"]
                  ]
   :profiles {:dev {:plugins [[lein-cljsbuild "1.0.3"]]}}
   :plugins [[datomic-schema-grapher "0.0.1"]]
@@ -21,6 +21,6 @@
         :source-paths ["src"]
         :compiler {
           :output-to "resources/public/js/main.js"
-          :optimizations :whitespace
-          :pretty-print true}}]}
+          :optimizations :advanced
+          :pretty-print false}}]}
   :ring {:handler kmg.core/app})
